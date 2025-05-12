ff61a18c0fe219ae5bb4dc231512e69d35c09d2f
urghosthost <132405002+urghosthost@users.noreply.github.com>
2025-05-12 17:52:04 +0000

    Create first program

---
+++ week1/01_first_program/first_program.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+// using directive 
+//using namespace std;
+
+int main() {
+    // Print a welcome message
+    std::cout << "Hello COP3337" << std::endl;
+
+    // cout - console output
+    //endl - new line, same as \n
+    // << stream insertion operator
+    // :: scope resolution operator
+    // std - standard namescape
+
+    std::cout << "5 / 2 is " << 5/2 << std::endl;
+    return 0;
+}
